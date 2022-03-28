1d6f839ae44a2c9494628c4d716da82de16c193e
vineeth konge <vineeth konge@DESKTOP-MQQN6CU>
2022-03-28 17:27:17 +0530

    myntra commit

---
+++ com.fashion.myntra/src/test/java/com/fashion/myntra/addcartTest/AddToCartTest.java
@@ -0,0 +1,11 @@
+package com.fashion.myntra.addcartTest;
+
+import org.testng.annotations.Test;
+
+public class AddToCartTest {
+	
+	@Test
+	public void sampleTest() {
+		System.out.println("pass");
+	}
+}
